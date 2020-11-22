df02dc86b5cafc4b01312ad1d068301f773dc506
hewi <h.wiechula@it-frankfurt.com>
2020-11-22 11:53:51 +0100

    First Draft

---
+++ src/app/model/TwitterSentiment.ts
@@ -0,0 +1,7 @@
+export class TwitterSentiment {
+    tweet: string;
+    airline_sentiment_predictions: string;
+    airline_sentiment_probabilities_negative: number;
+    airline_sentiment_probabilities_positive: number;
+    airline_sentiment_probability: number;
+}
